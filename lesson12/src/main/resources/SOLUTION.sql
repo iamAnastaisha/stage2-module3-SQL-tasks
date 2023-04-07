c4347bbfd5052cdfa87aab9cb5c4399b8a2e2325
Nastya Garkavaya <anastaishastil2002@gmail.com>
2023-04-07 18:33:56 +0300

    Commit 7

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 DELETE FROM student WHERE student.id IN (SELECT DISTINCT mark.student_id FROM mark INNER JOIN subject ON mark.subject_id = subject.id WHERE grade >= 4);
-DELETE FROM student WHERE student.id IN (SELECT DISTINCT id FROM student INNER JOIN mark ON student.id = mark.student_id WHERE mark < 4);
+DELETE FROM student WHERE student.id IN (SELECT DISTINCT student.id FROM student INNER JOIN mark ON student.id = mark.student_id WHERE mark < 4);
 DELETE FROM paymenttype WHERE paymenttype.name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
