cc771440e48bb4120f8a951f3b62b41fe8118b7c
Nastya Garkavaya <anastaishastil2002@gmail.com>
2023-04-07 18:43:10 +0300

    Commit 9

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,7 +2,7 @@ SELECT * FROM payment WHERE amount >= 500;
 SELECT * FROM student WHERE DATEADD(year, -20, CURRENT_DATE) > birthday;
 SELECT * FROM student WHERE groupnumber = 10 AND DATEADD(year, -20, CURRENT_DATE) < birthday;
 SELECT * FROM student WHERE student.name LIKE 'Mike%' OR groupnumber IN (4,5,6);
-SELECT * FROM payment WHERE DATEADD(month, -8, CURRENT_DATE) <= payment_date;
+SELECT * FROM payment WHERE DATEADD(month, -9, CURRENT_DATE) < payment_date;
 SELECT * FROM student WHERE student.name LIKE 'A%';
 SELECT * FROM student WHERE (student.name LIKE 'Roxi%' AND groupnumber = 4) OR (student.name LIKE 'Tallie%' AND groupnumber = 9);
 
